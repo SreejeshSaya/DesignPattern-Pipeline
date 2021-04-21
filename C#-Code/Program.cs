f369614f23b6cc085d0e0398c47ec8e686eae711
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2021-04-21 18:56:38 +0530

    Update README.md. Added C# code.

---
+++ C#-Code/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Helios.Architecture.Pipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Sequence.Test.Run();
+            Console.ReadLine();
+            Asynchronous.Test.Run();
+            Console.ReadLine();
+            Plugins.Test.Run();
+            Console.ReadLine();
+            Batch.Test.Run();
+            Console.ReadLine(); */
+
+            Processors.Test.Run();
+            Console.ReadLine();
+
+            /* Compiled.Benchmark.Run();
+            Console.ReadLine(); */
+            Console.WriteLine("Finished !");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DesignPattern-Pipeline
+
+## Pope Files
+[] CreateOrderProcessor.cs
+[] Processor.cs
+[] Pipeline.cs
+[] IOperation.cs
+[] Operation.cs
+
+## SJ Files
+[] DeliverOrderProcessor.cs
+[] PaymentOrderProcessor.cs
+[] PriceOrderProcessor.cs
+[] OrderStatus.cs
+[] Order.cs
+[] Product.cs
+
