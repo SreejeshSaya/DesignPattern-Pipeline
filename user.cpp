d6947ea2bdab619c795d5061ec014da41ca36aec
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2021-05-01 18:52:27 +0530

    Cleaned code in class User files

--- user.cpp
+++ user.cpp
@@ -5,3 +5,20 @@ std::vector<User> User::users_;
 User User::Alice((float)1000.00);
 User User::Bob((float)1000.00);
 User User::Charlie((float)1000.00);
+
+User::User(float b):
+    id(users_.size()), balance(b) {
+    users_.push_back(*this); 
+}
+
+
+float User::getBalance(unsigned int uid) {
+    if(uid > users_.size())
+        return 0;
+    return users_[uid].balance;
+}
+
+void User::updateBalance(unsigned int uid, float bal) {
+    if (uid < users_.size())
+        users_[uid].balance = bal;
+}
