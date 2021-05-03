411423d90e0f14c4a2fac3583de7c882338aba38
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2021-05-03 21:20:42 +0530

    Merged pipeline .cpp and .hpp file

--- pipeline.hpp
+++ pipeline.hpp
@@ -19,4 +19,35 @@ public:
 	void terminate();
 };
 
+template <typename T>
+Pipeline<T>::Pipeline() {}
+
+template <typename T>
+void Pipeline<T>::registerOperation(IOperation<T>* operation) {
+	if (!operations.empty()) {
+		operations.back()->Next = operation;
+	}
+	operations.push_back(operation);
+}
+
+template <typename T>
+void Pipeline<T>::invoke(T& data) {
+
+	for (auto operation : operations) {
+		operation->Terminate = operations.back();
+	}
+	operations.back()->Terminate = nullptr;
+	IOperation<Order>* op = (!operations.empty()) ? operations.front() : nullptr;
+	if (op == nullptr)
+		std::cout << "PIPELINE EMPTY!" << std::endl;
+	op->invoke(data);
+}
+
+template <typename T>
+void Pipeline<T>::terminate() {
+	for (IOperation<T>* operation : operations) {
+		operation->terminate();
+	}
+}
+
 #endif
