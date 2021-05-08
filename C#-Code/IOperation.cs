d818bea75e6a39ff7ef90258898575fb851e472e
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2021-05-08 18:00:06 +0530

    Removed C# code.

--- C#-Code/IOperation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Helios.Architecture.Pipeline.Processors
-{
-    public interface IOperation<T>
-    {
-        IOperation<T> Next { set; }
-        IOperation<T> Terminate { set; }
-        void Invoke(T data);
-    }
-}
