34a882f8c66b3341b31748add0bdce9dc275cd33
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2021-05-11 11:35:13 +0530

    Final to branch DP-changes

--- PaymentOrderProcessor.hpp
+++ PaymentOrderProcessor.hpp
@@ -12,14 +12,13 @@ protected:
         float balance = User::getBalance(order.userId);
         float expected = balance - order.totalPrice;
         if(expected >= 0) {
-            std::cout << "[PAYMENT] : Order {" << order.orderId << "} Paid\n" << std::endl;
+            std::cout << "PIPELINE:" << order.pNum << " --- [PAYMENT] : Order { " << order.orderId << "} Price: { " << order.totalPrice << " } Paid ---- Balance { " << expected << " }\n" << std::endl;
             User::updateBalance(order.userId, expected);
             order.status = Order::Paid;
             return true;
         }
         else {
             std::cout << "[PAYMENT] FAIL! : Order {" << order.orderId << "} Insufficient Balance for User " << order.userId << ": { " << balance << " } ; Need { " << (-1*expected) << " } more\n" << std::endl;
-            //std::cout<<"Insufficient Balance : User {"<<order.userId<<"} Balance {"<<balance<<"} USD | Order {"<<order.orderId<<"} : {"<<order.totalPrice<<"} USD\n" << std::endl;
             order.status = Order::Canceled;
             return false;
         }
