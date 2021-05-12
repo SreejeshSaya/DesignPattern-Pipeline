25e1d010f7cb27bd7eca820c2dc7ef734d34bd80
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2021-05-12 10:58:35 +0530

    Removed comment

--- ReportOrderProcessor.hpp
+++ ReportOrderProcessor.hpp
@@ -10,7 +10,6 @@ class ReportOrderProcessor : public Processor<Order> {
 protected:
 	bool Process(Order& order) {
 		unsigned int uid = order.userId;
-		// std::cout << "\n--- ORDER { " << order.orderId << " } INVOICE ---" << std::endl;
 		std::cout << "PIPELINE:" << order.pNum << " --- ACCOUNT DETAILS --- Order { " << order.orderId << " } \n";
 		User* user = User::getUser(uid);
 		std::cout << "User Details --- ";
